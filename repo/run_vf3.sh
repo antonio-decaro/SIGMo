be89fae9cab504039ecba2f0e05a25b97100e74c
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-07 14:48:20 +0100

    Add scripts to run benchmarks

---
+++ repo/run_vf3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Setting up variables
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATA_DIR=$SCRIPT_DIR/../data/vf3
+VF3_DIR=$SCRIPT_DIR/vf3
+OUT_DIR=$SCRIPT_DIR/../out
+EXEC_FILE=$VF3_DIR/bin/vf3l
+
+mkdir -p $OUT_DIR
+mkdir -p $OUT_DIR/vf3
+
+rm -f $OUT_DIR/vf3/vf3.out
+
+for data in $(ls $DATA_DIR/data)
+do
+  for query in $(ls $DATA_DIR/query)
+  do
+    echo "$data - $query" >> $OUT_DIR/vf3/vf3.out
+    out=$($EXEC_FILE $DATA_DIR/query/$query $DATA_DIR/data/$data | tail -1)
+    echo "$out" >> $OUT_DIR/vf3/vf3.out
+  done
+done
---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Setting up variables
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATA_DIR=$SCRIPT_DIR/data
+AVAILABLE_BENCHMARKS="msm,vf3"
+
+benchmarks=""
+
+help()
+{
+    echo "Usage: ./init_msm.sh 
+      [ -b=bench1,bench2,bench3] The set of benchmark files to be generated;
+      [ --data-limit= ] Limit the number of data files to be generated;
+      [ --query-limit= ] Limit the number of query files to be generated;
+      [ -h | --help ] Print this help message and exit.
+      The available benchmarks are: [msm, vf3]"
+}
+
+# Parsing arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -b=*)
+      benchmarks="${1#*=}"
+      shift
+      ;;
+    -h | --help)
+      help
+      exit 0
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      help
+      return 1 2>/dev/null
+      exit 1
+      ;;
+  esac
+done
+
+# check if benchmarks is empty and valid
+for bench in $(echo $benchmarks | sed "s/,/ /g")
+do
+  if [[ $AVAILABLE_BENCHMARKS != *"$bench"* ]]
+  then
+    echo "[!] Invalid benchmark: $bench"
+    return 1 2>/dev/null
+    exit 1
+  fi
+done
+if [ -z "$benchmarks" ]
+then
+  benchmarks=$AVAILABLE_BENCHMARKS
+fi
+
+for bench in $(echo $benchmarks | sed "s/,/ /g")
+do
+  # check if data directory exists
+  if [ ! -d "$DATA_DIR/$bench" ]
+  then
+    echo "[!] Data directory for $bench does not exist. Please run ./init.sh first."
+  else
+    $SCRIPT_DIR/repo/run_$bench.sh 
+  fi
+done
