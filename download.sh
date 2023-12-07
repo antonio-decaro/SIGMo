229969e176b81b51153d249919254dcb0758da46
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-07 17:11:16 +0100

    Fix download.sh script

--- download.sh
+++ download.sh
@@ -3,5 +3,6 @@
 git submodule update --init --recursive
 cd repo
 wget https://zenodo.org/records/5154114/files/CuTS-master.zip?download=1
-unzip CuTS-master.zip?download=1
-rm CuTS-master.zip?download=1
+mv CuTS-master.zip?download=1 CuTS-master.zip
+unzip CuTS-master.zip
+rm CuTS-master.zip
