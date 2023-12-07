1d74b60aa405446baea4c19201a849e5f4d495bf
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-07 13:25:24 +0100

    Update import and add getLabel function

--- scripts/utils/smarts2graph.py
+++ scripts/utils/smarts2graph.py
@@ -4,6 +4,8 @@ import pysmiles
 from rdkit import Chem
 import sys
 
+organic_subset = {l: (i+1) for i, l in enumerate('* H B C N O P S F Cl Br I b c n o s p'.split())}
+
 def smartsToGraph(smarts: str) -> nx.DiGraph:
   """Convert a SMARTS string to a networkx graph.
   
@@ -18,3 +20,7 @@ def smartsToGraph(smarts: str) -> nx.DiGraph:
   g: nx.Graph = pysmiles.read_smiles(mol_str, explicit_hydrogen=True)
   g = nx.DiGraph(g)
   return g
+
+def getLabel(g: dict, digit: bool = True):
+  return organic_subset.get(g.get('element', '*'), 0) if digit else str(g.get('element', '*'))
+  
