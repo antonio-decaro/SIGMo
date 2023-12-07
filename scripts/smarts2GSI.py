b11378691180c13bf12c0239fed2d550ee0472f8
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-07 16:16:30 +0100

    Update smarts2GSI.py script

--- scripts/smarts2GSI.py
+++ scripts/smarts2GSI.py
@@ -1,1 +1,21 @@
-# TODO
+#!/usr/bin/env python3
+import sys
+from utils import *
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print('Usage: python smarts2GSI.py <smarts>')
+    sys.exit(1)
+    
+  mol = sys.argv[1]
+  g = smartsToGraph(mol)
+  
+  print("t # 0")
+  print(len(g.nodes), len(g.edges), NUM_LABELS, 1)
+  for i in range(len(g.nodes)):
+    print('v', i, getLabel(g.nodes[i]) + 1)
+  
+  for u, v in g.edges:
+    print('e', u, v, 1)
+  
+  print('t # -1')
