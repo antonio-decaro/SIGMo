927a3c247b95b6acdf83accb010fd1a367b3ee8f
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-07 16:07:05 +0100

    Add build script for VF3 and GSI

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Setting up variables
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "[*] Building VF3"
+cd $SCRIPT_DIR/repo/vf3
+make
+
+echo "[*] Building GSI"
+cd $SCRIPT_DIR/repo/GSI
+# TODO
