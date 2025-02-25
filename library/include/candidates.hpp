5d989f6670ca6ad60a12bfd9dee19f68aca399f3
antonio-decaro <antonio.decaro99@outlook.it>
2025-02-25 15:35:34 +0000

    Refactor Candidates structure; rename query and data nodes to source and target nodes, and update related methods for consistency

--- library/include/candidates.hpp
+++ library/include/candidates.hpp
@@ -9,64 +9,64 @@ namespace candidates {
 struct Candidates {
   types::candidates_t* candidates;
   constexpr static types::candidates_t num_bits = sizeof(types::candidates_t) * 8;
-  size_t query_nodes;
-  size_t data_nodes;
+  size_t source_nodes;
+  size_t target_nodes;
   size_t single_node_size;
 
-  Candidates() : query_nodes(0), data_nodes(0), single_node_size(0) {}
+  Candidates() : source_nodes(0), target_nodes(0), single_node_size(0) {}
 
-  Candidates(size_t query_nodes, size_t data_nodes)
-      : query_nodes(query_nodes), data_nodes(data_nodes), single_node_size((data_nodes + (num_bits - 1)) / num_bits) {}
+  Candidates(size_t source_nodes, size_t target_nodes)
+      : source_nodes(source_nodes), target_nodes(target_nodes), single_node_size((target_nodes + (num_bits - 1)) / num_bits) {}
 
-  size_t getAllocationSize() const { return query_nodes * single_node_size * sizeof(types::candidates_t); }
+  size_t getAllocationSize() const { return source_nodes * single_node_size * sizeof(types::candidates_t); }
 
   void setDataCandidates(types::candidates_t* data_candidates) { candidates = data_candidates; }
 
-  SYCL_EXTERNAL void insert(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL void insert(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
-    candidates[query_node * this->single_node_size + idx] |= (static_cast<types::candidates_t>(1) << offset);
+    candidates[source_node * this->single_node_size + idx] |= (static_cast<types::candidates_t>(1) << offset);
   }
 
-  SYCL_EXTERNAL void atomicInsert(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL void atomicInsert(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
     sycl::atomic_ref<types::candidates_t, sycl::memory_order::relaxed, sycl::memory_scope::device> ref{
-        candidates[query_node * this->single_node_size + idx]};
+        candidates[source_node * this->single_node_size + idx]};
     ref |= (static_cast<types::candidates_t>(1) << offset);
   }
 
-  SYCL_EXTERNAL bool contains(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL bool contains(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
-    return (candidates[query_node * this->single_node_size + idx] & (static_cast<types::candidates_t>(1) << offset)) != 0;
+    return (candidates[source_node * this->single_node_size + idx] & (static_cast<types::candidates_t>(1) << offset)) != 0;
   }
 
-  SYCL_EXTERNAL bool atomicContains(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL bool atomicContains(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
     sycl::atomic_ref<types::candidates_t, sycl::memory_order::relaxed, sycl::memory_scope::device> ref{
-        candidates[query_node * this->single_node_size + idx]};
+        candidates[source_node * this->single_node_size + idx]};
     return (ref & (static_cast<types::candidates_t>(1) << offset)) != 0;
   }
 
-  SYCL_EXTERNAL void remove(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL void remove(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
-    candidates[query_node * this->single_node_size + idx] &= ~(static_cast<types::candidates_t>(1) << offset);
+    candidates[source_node * this->single_node_size + idx] &= ~(static_cast<types::candidates_t>(1) << offset);
   }
 
-  SYCL_EXTERNAL void atomicRemove(types::node_t query_node, types::node_t candidate) const {
+  SYCL_EXTERNAL void atomicRemove(types::node_t source_node, types::node_t candidate) const {
     types::candidates_t idx = candidate / num_bits;
     types::candidates_t offset = candidate % num_bits;
     sycl::atomic_ref<types::candidates_t, sycl::memory_order::relaxed, sycl::memory_scope::device> ref{
-        candidates[query_node * this->single_node_size + idx]};
+        candidates[source_node * this->single_node_size + idx]};
     ref &= ~(static_cast<types::candidates_t>(1) << offset);
   }
 
-  SYCL_EXTERNAL uint32_t getCandidatesCount(types::node_t query_node) const {
+  SYCL_EXTERNAL uint32_t getCandidatesCount(types::node_t source_node) const {
     uint32_t count = 0;
-    for (size_t i = 0; i < single_node_size; ++i) { count += sycl::popcount(candidates[query_node * single_node_size + i]); }
+    for (size_t i = 0; i < single_node_size; ++i) { count += sycl::popcount(candidates[source_node * single_node_size + i]); }
     return count;
   }
 };
