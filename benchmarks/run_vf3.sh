4f5987825c566b170564290b8b2ea40a519e4fec
antonio-decaro <antonio.decaro99@outlook.it>
2025-03-04 17:37:36 +0000

    Update run_vf3.sh to use consistent casing for output and directory names for improved clarity

--- benchmarks/run_vf3.sh
+++ benchmarks/run_vf3.sh
@@ -2,24 +2,24 @@
 
 # Setting up variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DATA_DIR=$SCRIPT_DIR/../data/vf3
+DATA_DIR=$SCRIPT_DIR/../data/VF3
 VF3_DIR=$SCRIPT_DIR/vf3
 OUT_DIR=$SCRIPT_DIR/../out
 EXEC_FILE=$VF3_DIR/bin/vf3l
 
 mkdir -p $OUT_DIR
-mkdir -p $OUT_DIR/vf3
+mkdir -p $OUT_DIR/VF3
 
-rm -f $OUT_DIR/vf3/vf3.out
+rm -f $OUT_DIR/VF3/VF3.out
 
 for data in $(ls $DATA_DIR/data)
 do
   for query in $(ls $DATA_DIR/query)
   do
-    echo "$data - $query" >> $OUT_DIR/vf3/vf3.out
+    echo "$data - $query" >> $OUT_DIR/VF3/VF3.out
     out=$($EXEC_FILE $DATA_DIR/query/$query $DATA_DIR/data/$data | tail -1)
-    echo "$out" >> $OUT_DIR/vf3/vf3.out
+    echo "$out" >> $OUT_DIR/VF3/VF3.out
   done
 done
 
-$SCRIPT_DIR/../scripts/parser/parseVF3.py $OUT_DIR/vf3/vf3.out $OUT_DIR/vf3/vf3.csv
+$SCRIPT_DIR/../scripts/parser/parseVF3.py $OUT_DIR/VF3/VF3.out $OUT_DIR/VF3/VF3.csv
